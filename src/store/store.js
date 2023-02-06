7250738dcc3685904bb63f17a8ad4db08dc2cd57
Hmmlov <miguelcondegarcia26@gmail.com>
2023-02-06 08:49:05 -0500

    ConfiguracionDelAuthSlice

---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'
+/* importado de la carpeta auth/index.js */
+import { authSlice } from './auth' // hace referencia al archivo barril index.js
+
+export const store = configureStore({
+  reducer: {
+    auth: authSlice.reducer,
+  },
+})
